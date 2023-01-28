dd82f65f60584b4e918225eec4c60a5a1d9c2f73
KEENAN MCGUCKIN <keenan.mcg@gmail.com>
2023-01-28 15:48:37 -0800

    HTML/CSS/JS updates

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+/* eslint-disable no-console */
 // grab DOM elements
 
 // set event listeners
+console.log('Selam näw, agärewa');
+console.log(42 + 42);
+
+const dogName = 'Sheila';
+console.log(`My dog\'s name is ${dogName}. ${dogName} is 3 years old. ${dogName} is s\'cute`);
+
+function tellMeImDumb(name) {
+    console.log(`ur dum, ${name}`);
+}
+
+tellMeImDumb('Keenan');
