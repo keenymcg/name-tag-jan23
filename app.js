5116889974a080796baba9b801a6e9dd5c8af5f2
KEENAN MCGUCKIN <keenan.mcg@gmail.com>
2023-01-28 16:43:38 -0800

    the name tag updates based on the name input v2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log('Selam näw, agärewa');
 console.log(42 + 42);
 
 const dogName = 'Sheila';
-console.log(`My dog\'s name is ${dogName}. ${dogName} is 3 years old. ${dogName} is s\'cute`);
+console.log(`My dog's name is ${dogName}. ${dogName} is 3 years old. ${dogName} is s'cute`);
 
 function tellMeImDumb(name) {
     console.log(`ur dum, ${name}`);
